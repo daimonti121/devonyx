d2aff97402b871437f0ced1030a9f17a268ae33d
Yushenko Dima <damios.1812@gmail.com>
2023-07-29 18:39:02 +0300

    fix 5

---
+++ js/photo/logicPhoto.js
@@ -0,0 +1,31 @@
+document.querySelector('button.image-1').onclick = () => {
+
+    basicLightbox.create(`
+        <img style="max-width: 95%;" class="big-image-1" src="../../img/big-photo-1.jpg">
+    `).show()
+
+}
+
+document.querySelector('button.image-2').onclick = () => {
+
+    basicLightbox.create(`
+        <img style="max-width: 95%;" class="big-image-1" src="../../img/big-photo-2.jpg">
+    `).show()
+
+}
+
+document.querySelector('button.image-3').onclick = () => {
+
+    basicLightbox.create(`
+        <img style="max-width: 95%;" class="big-image-1" src="../../img/big-photo-3.jpg">
+    `).show()
+
+}
+
+document.querySelector('button.image-4').onclick = () => {
+
+    basicLightbox.create(`
+        <img style="max-width: 95%;" class="big-image-1" src="../../img/big-photo-4.jpg">
+    `).show()
+
+}
