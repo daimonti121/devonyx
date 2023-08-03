3e69a9e4e327d84580b060cfc9c3bf8d1058f7e1
Yushenko Dima <damios.1812@gmail.com>
2023-08-03 22:01:51 +0300

    fix 7

--- js/photo/logicPhoto.js
+++ js/photo/logicPhoto.js
@@ -1,7 +1,7 @@
 document.querySelector('button.image-1').onclick = () => {
 
     basicLightbox.create(`
-        <img style="max-width: 95%; border-radius: 20px;" class="big-image-1" src="../../img/big-photo-1.jpg">
+        <img style="max-width: 95%; border-radius: 20px;" class="big-image-1" src="../img/big-photo-1.jpg">
     `).show()
 
 }
@@ -9,7 +9,7 @@ document.querySelector('button.image-1').onclick = () => {
 document.querySelector('button.image-2').onclick = () => {
 
     basicLightbox.create(`
-        <img style="max-width: 95%; border-radius: 20px;" class="big-image-1" src="../../img/big-photo-2.jpg">
+        <img style="max-width: 95%; border-radius: 20px;" class="big-image-1" src="../img/big-photo-2.jpg">
     `).show()
 
 }
@@ -17,7 +17,7 @@ document.querySelector('button.image-2').onclick = () => {
 document.querySelector('button.image-3').onclick = () => {
 
     basicLightbox.create(`
-        <img style="max-width: 95%; border-radius: 20px;" class="big-image-1" src="../../img/big-photo-3.jpg">
+        <img style="max-width: 95%; border-radius: 20px;" class="big-image-1" src="../img/big-photo-3.jpg">
     `).show()
 
 }
@@ -25,7 +25,7 @@ document.querySelector('button.image-3').onclick = () => {
 document.querySelector('button.image-4').onclick = () => {
 
     basicLightbox.create(`
-        <img style="max-width: 95%; border-radius: 20px;" class="big-image-1" src="../../img/big-photo-4.jpg">
+        <img style="max-width: 95%; border-radius: 20px;" class="big-image-1" src="../img/big-photo-4.jpg">
     `).show()
 
 }
